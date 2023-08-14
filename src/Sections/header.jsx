84862df378f3be2bb6ca5cc98dea5c443f245b04
ShireenAtarod <at.shi.1989@gmail.com>
2023-08-14 14:11:57 +0330

    feature: add news cards

---
+++ src/Sections/header.jsx
@@ -0,0 +1,12 @@
+import BlackBox from "../components/BlackBox";
+import { Card } from "@mui/material";
+
+export default function Header() {
+    return(
+        <Card className="header">
+            <BlackBox>
+                
+            </BlackBox>
+        </Card>
+    )
+};
