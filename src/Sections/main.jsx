333b9375a3cf44f36cc40ff1592f4afeae8c36d4
ShireenAtarod <at.shi.1989@gmail.com>
2023-09-02 13:00:15 +0330

    use of useRef

--- src/Sections/main.jsx
+++ src/Sections/main.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect, useCallback } from "react";
-import { Grid, Box, Tabs, Tab } from "@mui/material";
+import { useState, useEffect, useCallback, useRef } from "react";
+import { Grid, Box, Tabs, Tab, TextField, Button } from "@mui/material";
 import NewsCard from '../components/NewsCard';
 import axios from '../services/axios';
 
@@ -7,9 +7,13 @@ import axios from '../services/axios';
 export default function Main() {
     const categories = ['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology']
 
+    const searchRef = useRef(null);
+    const renderCount = useRef(0);
     const [newsList, setNewsList] = useState(null);
     const [tabValue, setTabValue] = useState(0);
     const [reload, setReload] = useState(true);
+
+    renderCount.current++;
 
     const handleTabChange = (event, newValue) => {
         setTabValue(newValue);
@@ -18,12 +22,12 @@ export default function Main() {
 
     const getNews = useCallback(async () => {
         try{
-            const response = await axios.get('/ne');
+            const response = await axios.get('/news');
             console.log(response);
             setReload(!reload);
             setNewsList(response.data.data)
         } catch(error){
-            if (error.response.status === 500) {
+            if (error.response.status === 403) {
                 window.location.replace("/error")
             }
         }
@@ -33,8 +37,18 @@ export default function Main() {
         getNews();
     }, [tabValue])
 
+    useEffect(() => {
+        searchRef.current.focus();
+    }, [])
+
     return(
         <>
+            {console.log(1)}
+            <p style={{display: 'flex', left: '0', marginTop: '8vh'}}>Render Count: {renderCount.current}</p>
+            <Box sx={{display: 'flex'}}>
+                <TextField label="search" variant="outlined" inputRef={searchRef} sx={{float: 'left'}} /> 
+                <Button variant="contained" sx={{float: 'left', margin: '1px 5px'}} onClick={()=>setReload(!reload)}>Search</Button>
+            </Box>
             <Tabs value={tabValue} onChange={handleTabChange} sx={{marginTop: "10vh"}}>
                 {categories.map((category) => {
                     return(
--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -1,7 +1,6 @@
 // import { Svg404 } from "../statics/iconSvg"
 import { ReactComponent as Icon404} from '../assets/Icon404.svg'
 export default function Error404() {
-    console.log(Icon404)
     return(
         <div style={{width: '70vw', margin: "auto"}}>
             <Icon404 />
